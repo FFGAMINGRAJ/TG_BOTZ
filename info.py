fda1d1b06195e0a587c88d3279f0eed721c01979
FFGAMINGRAJ <98765020+FFGAMINGRAJ@users.noreply.github.com>
2024-07-31 20:37:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', True))
+VERIFY = bool(environ.get('VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'publicearn.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'a9f04899e7861f97eef19a105f34005bd320137c')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
