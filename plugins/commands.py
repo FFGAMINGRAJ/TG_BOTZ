bf9380c2b8097b4ce62103360a1302def453ee86
FFGAMINGRAJ <98765020+FFGAMINGRAJ@users.noreply.github.com>
2024-07-31 23:33:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -413,7 +413,7 @@ async def start(client, message):
             )
             filetype = msg.media
             file = getattr(msg, filetype.value)
-            title = '@beautyofthemovie  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), file.file_name.split()))
+            title = '@NEW_ANIMES_DUBBED_HINDI  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), file.file_name.split()))
             size=get_size(file.file_size)
             f_caption = f"<code>{title}</code>"
             if CUSTOM_FILE_CAPTION:
