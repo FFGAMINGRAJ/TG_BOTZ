3e34847737e93ff0cc9ab290cacb879c6133e681
FFGAMINGRAJ <98765020+FFGAMINGRAJ@users.noreply.github.com>
2025-07-31 21:42:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '16986880'))
 API_HASH = environ.get('API_HASH', '535a942effdf0813faccab6ca5d2db41')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6755237545:AAFUOwxsKlbqP6fqrzSkjboPw4Dl04oLT-g")
+BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -43,7 +43,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://vk4055259:ov0nsifXfQqXk7cp@cluster0.ket1uaz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "vk4055259")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
